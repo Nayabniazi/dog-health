2a624290ed373837a5cd8a012e3c840dbd374b48
sheikhmuhammadzain <zaingee320@gmail.com>
2026-01-15 16:31:47 +0500

    aded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
-import { Header } from "@/components/layout/Header";
+import { HeaderWrapper } from "@/components/layout/HeaderWrapper";
 import "./globals.css";
 
 const inter = Inter({
@@ -51,7 +51,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <body className="antialiased">
-        <Header />
+        <HeaderWrapper />
         {children}
       </body>
     </html>
